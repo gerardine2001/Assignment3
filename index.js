10a38fb32f2ab7d19191a5246fe798a6483d5940
gerardine <gerardinekaba@gmail.com>
2021-03-22 07:18:13 +0300

    Assignment3

---
+++ index.js
@@ -0,0 +1,27 @@
+function getGrade(marks){
+    var marks=84;
+    if (marks>=80){ 
+    console.log("A");
+    }  
+    else if(marks>=70){ 
+var b=(70-79);
+    console.log("B")
+    }
+    else if(marks>=60){
+     var c=(60-69);   
+    console.log("C")
+    }
+    else if(marks>=50){
+     var d=(50-59);    
+    console.log("D") 
+
+    }
+    else if(marks>=40){ 
+        var e=(40-49);       
+         console.log("E")
+    }
+    else if("something else"){
+    }  
+    }
+    getGrade ();
+
